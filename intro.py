6b7922a119ddb722a9cd716edff518e0493f2f65
acarmena <camond87979@uat.edu>
2025-10-29 13:55:46 -0700

    Add files via upload

---
+++ intro.py
@@ -0,0 +1,14 @@
+print("Hi, welcome to a Python tutorial!")
+print()
+
+print("Today, we are going to learn how to take user inputs!")
+print()
+
+input("To begin, what do you think you need to type? ")
+
+if(input == "input")
+    print("Correct! You would type this for example: input blah blah")
+else
+    print("Wrong, nuit that is why we are leaning!")
+
+input("name? ")

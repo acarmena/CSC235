f4dd36a91485bdd68164932080a6cf8421d1a672
acarmena <camond87979@uat.edu>
2025-10-31 13:22:28 -0700

    Add files via upload

--- intro.py
+++ intro.py
@@ -4,11 +4,29 @@ print()
 print("Today, we are going to learn how to take user inputs!")
 print()
 
-input("To begin, what do you think you need to type? ")
+ans = input("To begin, what do you think you need to type? ")
 
-if(input == "input")
-    print("Correct! You would type this for example: input blah blah")
-else
-    print("Wrong, nuit that is why we are leaning!")
+if ans == "input" :
+    print("Correct! You would type this for example: input('blah blah ')")
+    print()
+else:
+    print("Wrong, but! that is why we are learning!")
+    print("To ask for a user input, you need to type this 'input('whatever you want to ask the user')'")
+    print()
 
-input("name? ")
+
+print("If you want to save the input on a variable you would do that same but set the input equal to a variable name.")
+print()
+
+print("Let's try!")
+name = input("Enter your name: ")
+print()
+
+print("You notice how the code prompted you to enter answer earlier and just now?")
+print("yeah! that is how the user enters information.")
+print()
+
+print("Now if i wanted to print it as a variable I would say: 'variable = input('blah blah')'")
+print("Then print the variable name.")
+print("Like this:")
+print(name + ". See how the name you entered earlier is printed again, without you typing again?")

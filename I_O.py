4e4e97770ff5a309057c5d74911b9905b8bdb487
acarmena <camond87979@uat.edu>
2025-11-03 14:05:20 -0700

    Add files via upload

---
+++ I_O.py
@@ -0,0 +1,19 @@
+# In this fun assignment, we will learn about variables and data types, along with user input and output. We will also learn how to read and write files in Python.
+
+# Guidelines:
+# - Clearly demonstrate your mastery over input and output to the user while holding different types and sizes of data in variables.
+# - Think of how data persistence would benefit the UX of your App.
+# - What data would you like your program to persist between uses?
+# - Maybe Save the Game, Save inventory, Save user Info?
+
+# Requirements:
+# Add user input and output to your application.
+# Add at least two variables holding different data types to your application.
+# Write data to a text file.
+# Read data from a text file.
+# Your read and writes should add something useful to your application, not just demo how to read and write to and from a text file.
+# Overcomment your source code file (.py) well.
+# This program should have a great user experience.
+# OPTIONAL NOT REQUIRED: Use functions in your program
+# 
+# Theme: blah blah.
